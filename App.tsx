e6f16b3dd7960b3326d7421720388610f9c4c222
manu mathew <manu@easyecom.io>
2024-02-10 14:46:22 +0530

    init

--- App.tsx
+++ App.tsx
@@ -5,40 +5,39 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import type { PropsWithChildren } from "react";
+import React, { useState } from "react";
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View } from "react-native";
 
 import {
   Colors,
   DebugInstructions,
   Header,
   LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  ReloadInstructions
+} from "react-native/Libraries/NewAppScreen";
+import { LivelyShoppableFeeds } from "lively-feeds-rn";
+
 
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+export const CART = {
+  "items": [],
+  "items_subtotal_price": 579600
+};
+
+function Section({ children, title }: SectionProps): React.JSX.Element {
+  const isDarkMode = useColorScheme() === "dark";
   return (
     <View style={styles.sectionContainer}>
       <Text
         style={[
           styles.sectionTitle,
           {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
+            color: isDarkMode ? Colors.white : Colors.black
+          }
         ]}>
         {title}
       </Text>
@@ -46,8 +45,8 @@ function Section({children, title}: SectionProps): React.JSX.Element {
         style={[
           styles.sectionDescription,
           {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
+            color: isDarkMode ? Colors.light : Colors.dark
+          }
         ]}>
         {children}
       </Text>
@@ -56,16 +55,17 @@ function Section({children, title}: SectionProps): React.JSX.Element {
 }
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
+  const isDarkMode = useColorScheme() === "dark";
+  const [cartDetails, setCartDetails] = useState(CART);
+  const [load, setLoading] = useState(false);
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter
   };
 
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        barStyle={isDarkMode ? "light-content" : "dark-content"}
         backgroundColor={backgroundStyle.backgroundColor}
       />
       <ScrollView
@@ -74,12 +74,46 @@ function App(): React.JSX.Element {
         <Header />
         <View
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            backgroundColor: isDarkMode ? Colors.black : Colors.white
           }}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
+
+          <LivelyShoppableFeeds
+            livelyBrandId="57bf2ef4b6"
+            onCartUpdate={async (type, data) => {
+              setLoading(true);
+              const newItem = {
+                "quantity": 1,
+                "variant_id": 123456789,
+                "product_title": "New Product",
+                "line_price": 1000,
+                "image": "https://example.com/new-product.jpg",
+              };
+
+              // Update the cart state based on the current state
+              setCartDetails(prevCart => ({
+                ...prevCart,
+                items: [...prevCart.items, newItem],
+              }));
+
+              await new Promise(resolve => setTimeout(resolve, 3000));
+
+              setLoading(false);
+            }}
+            cartDetails={cartDetails}
+            onShopNow={() => {
+              console.log("SHOP NOW CLICKED GO TO CHECKOUT PAGE")
+            }}
+            callBacks={(event, data) => {
+              console.log(event);
+              console.log(data);
+            }}
+            isLoading={load}
+            type="home"
+          />
           <Section title="See Your Changes">
             <ReloadInstructions />
           </Section>
@@ -99,20 +133,20 @@ function App(): React.JSX.Element {
 const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
-    paddingHorizontal: 24,
+    paddingHorizontal: 24
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: '600',
+    fontWeight: "600"
   },
   sectionDescription: {
     marginTop: 8,
     fontSize: 18,
-    fontWeight: '400',
+    fontWeight: "400"
   },
   highlight: {
-    fontWeight: '700',
-  },
+    fontWeight: "700"
+  }
 });
 
 export default App;
---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dependencies: {
+    'react-native-video': {},
+    'react-native-fast-image':{},
+    'react-native-linear-gradient':{},
+    'react-native-render-html':{},
+    'react-native-svg':{},
+
+  }
+};
